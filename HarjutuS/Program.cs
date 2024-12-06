2062429019d5f1164641bdc5336eef94f406a74a
Blokewed <peeter.sture@tktk.ee>
2024-12-06 12:32:41 +0200

    Week3

---
+++ HarjutuS/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Diagnostics;
+
+Console.WriteLine("Mis sa hindeks said?");
+char grade;
+
+grade = Convert.ToChar(Console.ReadLine().ToLower());
+
+
+if (grade == 'a')
+{
+    Console.WriteLine("Suurepärane!");
+}
+else if (grade == 'b')
+{
+    Console.WriteLine("Väga hea!!");
+}
+else if (grade == 'c')
+{
+    Console.WriteLine("Hea!");
+}
+else if (grade == 'd')
+{
+    Console.WriteLine("Rahuldav");
+}
+else if (grade == 'e')
+{
+    Console.WriteLine("Kasin");
+}
+else if (grade == 'f')
+{
+    Console.WriteLine("Puudulik");
+}
+else
+{
+    Console.WriteLine("Vale väärtus!");
+}
+
+
+
+
+
